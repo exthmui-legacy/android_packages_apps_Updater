e7872053319a4a59a9b97185b41881a535b80d27
ISNing <ISNing@exthmui.cn>
2020-03-12 13:41:14 +0000

    Updater:fix:broken makefile

--- Android.mk
+++ Android.mk
@@ -36,13 +36,10 @@ updater_system_libs_path := $(abspath $(LOCAL_PATH))/system_libs
 
 include $(BUILD_SYSTEM)/base_rules.mk
 
-.PHONY: copy_updater_system_deps
-copy_updater_system_deps: $(updater_system_deps)
+$(LOCAL_BUILT_MODULE): $(updater_system_deps)
 	$(hide) mkdir -p $(updater_system_libs_path)
 	$(hide) rm -rf $(updater_system_libs_path)/*.jar
 	$(hide) cp $(updater_system_deps) $(updater_system_libs_path)/framework.jar
-
-$(LOCAL_BUILT_MODULE): copy_updater_system_deps
 	$(hide) echo "Fake: $@"
 	$(hide) mkdir -p $(dir $@)
 	$(hide) touch $@
