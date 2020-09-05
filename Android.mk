123137bdc5070072c7277e2a28090d4ce559fe28
Color-yourself <1638876934@qq.com>
2020-09-06 04:11:02 +0800

    Update Android.mk
    Updater: fix a build error in makefile

--- Android.mk
+++ Android.mk
@@ -15,7 +15,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 	androidx.cardview_cardview \
 	androidx.preference_preference \
 	androidx.recyclerview_recyclerview \
-	androidx.constraintlayout_constraintlayout \
+	androidx-constraintlayout_constraintlayout \
 	androidx.localbroadcastmanager_localbroadcastmanager
 
 LOCAL_RESOURCE_DIR := \
